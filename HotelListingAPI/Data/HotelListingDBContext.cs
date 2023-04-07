19063d44775fcdc715287bf092125accbea69512
Jose Mendez <jose.mendez@gorillalogic.com>
2023-04-07 10:44:10 -0600

    added EF and seeded hotels and countries

---
+++ HotelListingAPI/Data/HotelListingDBContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+	public class HotelListingDBContext : DbContext
+	{
+		public HotelListingDBContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Hotel> Hotels { get; set; }
+		public DbSet<Country> Countries { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Country>()
+				.Property(e => e.Name)
+				.HasMaxLength(50); // Set the maximum length of the property to 50 characters
+            modelBuilder.Entity<Country>()
+                .Property(e => e.ShortName)
+                .HasMaxLength(50); // Set the maximum length of the property to 50 characters
+            modelBuilder.Entity<Hotel>()
+                .Property(e => e.Name)
+                .HasMaxLength(100); // Set the maximum length of the property to 100 characters
+            modelBuilder.Entity<Hotel>()
+                .Property(e => e.Address)
+                .HasMaxLength(50); // Set the maximum length of the property to 50 characters
+
+
+            modelBuilder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Jamaica",
+                        ShortName = "JM"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Bahamas",
+                        ShortName = "BS"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Cayman Island",
+                        ShortName = "CI"
+                    }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                    new Hotel
+                    {
+                        Id = 1,
+                        Name = "Sandals Resort and Spa",
+                        Address = "Negril",
+                        CountryId = 1,
+                        Rating = 4.5
+                    },
+                    new Hotel
+                    {
+                        Id = 2,
+                        Name = "Comfort Suites",
+                        Address = "George Town",
+                        CountryId = 3,
+                        Rating = 4.3
+                    },
+                    new Hotel
+                    {
+                        Id = 3,
+                        Name = "Grand Palldium",
+                        Address = "Nassua",
+                        CountryId = 2,
+                        Rating = 4
+                    }
+                );
+        }
+	}
+}
+
--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,11 +1,28 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using HotelListingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Microsoft.Data.Sqlite;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var connectionString = new SqliteConnection($"Data Source=C:\\hotellistdb\\hotellist.db");
+builder.Services.AddDbContext<HotelListingDBContext>(o => o.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+// ctx =context, lc = logger configuration
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
@@ -16,7 +33,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// log the type of requests coming in and how long they took to response
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
